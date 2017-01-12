703ccecc701879aaa33779be2cc50d79476c58f2
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2017-01-11 22:44:00 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello Quay!!")
+	io.WriteString(w, "<font size=\"8\">Hello CodeMash!!</font>")
 }
 
 func main() {
