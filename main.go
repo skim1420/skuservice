4a363ac328095cdebba0fb4fe14cb6b048ff93b6
rguthriemsft <rguthrie@Microsoft.com>
2017-02-02 06:53:23 -0800

    resetting demo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<font size=\"8\">Hello Commercial team!!</font>")
+	io.WriteString(w, "<font size=\"8\">Hello World!!</font>")
 }
 
 func main() {
