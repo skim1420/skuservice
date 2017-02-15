664a11bbc0f494f67834fd351c09208fd73ddf4d
rguthriemsft <rguthrie@Microsoft.com>
2017-02-15 13:46:43 -0800

    demo script

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<font size=\"8\">Hello World test!!</font>")
+	io.WriteString(w, "<font size=\"8\">Hello Damien!!</font>")
 }
 
 func main() {
